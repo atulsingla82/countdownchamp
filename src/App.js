ca000476f74770c10c3e6cd09f997715c5c18948
atulsingla82 <asingla@wgu.edu>
2017-09-01 23:59:42 -0400

    Testing out Sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React,{ Component } from 'react';
+import './App.css';
 
 export default class App extends Component {
   
    render(){
     return(
-    <div> Countdown Champ, App Component </div>
+    <div className="App"> 
+    <div className="App-title"> Countdown Timer </div>
+    <div>
+    <div className="Clock-days"> Days: </div>
+    <div className="Clock-hours"> Hours:</div>
+    <div className="Clock-minutes"> Minutes:</div>
+    <div className="Clock-seconds"> Seconds:</div>
+    </div>
+
+    <div>
+    <input placeholder='newdate' />
+    <button> Submit </button>
+    </div>
+
+     </div>
    )
    }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './App.scss';
 
 import App from './App'
 ReactDom.render(
